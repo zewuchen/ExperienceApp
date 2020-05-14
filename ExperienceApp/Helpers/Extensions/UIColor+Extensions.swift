b6c3dd469d584e69110c3ee9bdb56dfa6ae302c1
Zewu Chen <zewu.chen@etec.sp.gov.br>
2020-05-14 15:51:28 -0300

    adicionado as extensions

---
+++ ExperienceApp/Helpers/Extensions/UIColor+Extensions.swift
@@ -0,0 +1,27 @@
+//
+//  UIColor+Extensions.swift
+//  ExperienceApp
+//
+//  Created by Zewu Chen on 14/05/20.
+//  Copyright © 2020 Zewu Chen. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static var color1: UIColor {
+        return #colorLiteral(red: 0.5960784314, green: 0.8901960784, blue: 0.568627451, alpha: 1)
+    }
+
+    static var color2: UIColor {
+        return #colorLiteral(red: 0.8901960784, green: 0.568627451, blue: 0.568627451, alpha: 1)
+    }
+
+    static var color3: UIColor {
+        return #colorLiteral(red: 0.2274509804, green: 0.2274509804, blue: 0.2274509804, alpha: 1)
+    }
+
+    static var color4: UIColor {
+        return #colorLiteral(red: 0.8509803922, green: 0.8509803922, blue: 0.8509803922, alpha: 1)
+    }
+}
