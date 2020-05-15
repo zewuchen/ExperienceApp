b8fde2acf1052b8cea67b25b89044938f329ee8d
Juliana Vigato Pavan <ju.vigato3@gmail.com>
2020-05-15 15:47:21 -0300

    Criando xib das celulas

---
+++ ExperienceApp/Screen - Main/View/Cells/DestaquesCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  DestaquesCollectionViewCell.swift
+//  ExperienceApp
+//
+//  Created by Juliana Vigato Pavan on 15/05/20.
+//  Copyright © 2020 Zewu Chen. All rights reserved.
+//
+
+import UIKit
+
+class DestaquesCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
