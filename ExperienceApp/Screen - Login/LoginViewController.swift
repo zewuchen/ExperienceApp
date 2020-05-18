afe9f2901ca07507b0ae2c841ba1d1bd0f7588ca
Zewu Chen <zewu.chen@etec.sp.gov.br>
2020-05-18 19:18:14 -0300

    Fixed error with load NIB

--- ExperienceApp/Screen - Login/LoginViewController.swift
+++ ExperienceApp/Screen - Login/LoginViewController.swift
@@ -15,13 +15,13 @@ class LoginViewController: UIViewController {
     }
 
     @IBAction func btnLoginScreen(_ sender: Any) {
-        let novaTela = CriarContaViewController(nibName: "loginScreen", bundle: nil)
+        let novaTela = CriarContaViewController(nibName: "CriarContaViewController", bundle: nil)
 //        novaTela.modalPresentationStyle = .fullScreen
         self.present(novaTela, animated: true, completion: nil)
     }
     
     @IBAction func btnLoginCreate(_ sender: Any) {
-        let novaTela = EntrarNaContaViewController(nibName: "createScreen", bundle: nil)
+        let novaTela = EntrarNaContaViewController(nibName: "EntrarNaContaViewController", bundle: nil)
 //        novaTela.modalPresentationStyle = .fullScreen
         self.present(novaTela, animated: true, completion: nil)
     }
