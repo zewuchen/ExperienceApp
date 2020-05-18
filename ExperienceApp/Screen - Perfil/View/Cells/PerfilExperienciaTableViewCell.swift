ea1071a36cb57449b253e90db2db2696da65ab62
Nathalia <nathaliamelare@gmail.com>
2020-05-18 16:38:02 -0300

    Add UI do perfil e os outlets

---
+++ ExperienceApp/Screen - Perfil/View/Cells/PerfilExperienciaTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PerfilExperienciaTableViewCell.swift
+//  ExperienceApp
+//
+//  Created by Nathalia Melare on 18/05/20.
+//  Copyright © 2020 Zewu Chen. All rights reserved.
+//
+
+import UIKit
+
+class PerfilExperienciaTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+    
+}
--- ExperienceApp/Screen - Perfil/View/PerfilViewController.swift
+++ ExperienceApp/Screen - Perfil/View/PerfilViewController.swift
@@ -9,22 +9,18 @@
 import UIKit
 
 class PerfilViewController: UIViewController {
+    
+    @IBOutlet weak var perfilImage: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var biographyTitle: UILabel!
+    @IBOutlet weak var biographyText: UITextView!
+    @IBOutlet weak var experienceTitle: UILabel!
+    @IBOutlet weak var experienceTableView: UITableView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
