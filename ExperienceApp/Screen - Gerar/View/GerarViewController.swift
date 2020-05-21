d6e5ef807f2bed432141bba095356e1a07781f50
Juliana Vigato Pavan <ju.vigato3@gmail.com>
2020-05-21 15:35:15 -0300

    validando campos

--- ExperienceApp/Screen - Gerar/View/GerarViewController.swift
+++ ExperienceApp/Screen - Gerar/View/GerarViewController.swift
@@ -19,7 +19,11 @@ class GerarViewController: UIViewController {
     @IBOutlet weak var txtParticipar: UITextField!
 
     private let controller = GerarController()
-
+    let corBordaCerta = UIColor.lightGray.cgColor
+    let corBordaErrada = UIColor.init(red: 1.83, green: 0.77, blue: 0.77, alpha: 1.0).cgColor
+    var arrayReturn: [Bool] = []
+    let border = 0.25
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         controller.delegate = self
@@ -30,17 +34,17 @@ class GerarViewController: UIViewController {
     }
 
     @IBAction func btnCreateExperience(_ sender: Any) {
-        
         if validate() {
             guard let title = txtTema.text else { return }
             guard let description = txtDescription.text else { return }
             guard let tamanho = txtTamanho.text else { return }
             guard let lengthGroup = Int64(tamanho) else { return }
             guard let howToParticipate = txtParticipar.text else { return }
-            guard let whatToTake = txtParticipar.text else { return }
+            guard let whatToTake = txtRecursos.text else { return }
             
             let experience = ExperienceModel(title: title, description: description, howToParticipate: howToParticipate, lengthGroup: lengthGroup, whatToTake: whatToTake)
-
+            
+            
             controller.createExperience(data: experience)
         }
     }
@@ -56,18 +60,103 @@ class GerarViewController: UIViewController {
     
     func validate() -> Bool {
         // TODO: Validar os campos de dados aqui
-        let corBorda = UIColor.red.cgColor
-        let corBorda2 = UIColor.init(red: 1.83, green: 0.77, blue: 0.77, alpha: 1.0).cgColor
+        arrayReturn = []
         
-        if txtTema.text == "" || txtTema.text == nil {
-            print("faltam dados")
-            
-            txtTema.layer.borderColor = corBorda2
+        // Validando Texto Tema
+        if txtTema.text == "" || txtTema.text == nil || txtTema.text?.count ?? 1 > 50 {
+            txtTema.layer.borderColor = corBordaErrada
             txtTema.layer.borderWidth = 2.0
             txtTema.layer.cornerRadius = 6
-            return false
+            arrayReturn.append(false)
+        } else {
+            txtTema.layer.borderColor = corBordaCerta
+            txtTema.layer.borderWidth = 0.25
+            arrayReturn.append(true)
         }
-        return true
+        
+        
+        // Validando Texto Descrição
+        // TODO: quantidade de caracteres
+        if txtDescription.text == "" || txtDescription.text == nil || txtDescription.text?.count ?? 1 > 240 {
+            txtDescription.layer.borderColor = corBordaErrada
+            txtDescription.layer.borderWidth = 2.0
+            txtDescription.layer.cornerRadius = 6
+            arrayReturn.append(false)
+        } else {
+            txtDescription.layer.borderColor = corBordaCerta
+//            print(txtDescription.text?.count ?? "juu")
+            txtDescription.layer.borderWidth = 0.25
+            arrayReturn.append(true)
+        }
+        
+        
+        // Validando Texto Tamanhos
+        let validacaoTamanho = verificarTam()
+        arrayReturn.append(validacaoTamanho)
+
+        if let num = Int(txtTamanho.text!) {
+            if num > 50 || num <= 0 {
+                txtTamanho.layer.borderColor = corBordaErrada
+                txtTamanho.layer.borderWidth = 2.0
+                txtTamanho.layer.cornerRadius = 6
+                arrayReturn.append(false)
+            } else {
+                txtTamanho.layer.borderColor = corBordaCerta
+                txtTamanho.layer.borderWidth = 0.25
+                arrayReturn.append(true)
+            }
+        }
+        
+        
+        // Validando Texto HowToParticipate
+        if txtParticipar.text == "" || txtParticipar.text == nil || txtParticipar.text?.count ?? 1 > 240 {
+            txtParticipar.layer.borderColor = corBordaErrada
+            txtParticipar.layer.borderWidth = 2.0
+            txtParticipar.layer.cornerRadius = 6
+            arrayReturn.append(false)
+        } else {
+            txtParticipar.layer.borderColor = corBordaCerta
+            txtParticipar.layer.borderWidth = 0.25
+            arrayReturn.append(true)
+        }
+        
+        // Validando Texto Recursos
+        if txtRecursos.text == "" || txtRecursos.text == nil || txtRecursos.text?.count ?? 1 > 240 {
+            txtRecursos.layer.borderColor = corBordaErrada
+            txtRecursos.layer.borderWidth = 2.0
+            txtRecursos.layer.cornerRadius = 6
+            arrayReturn.append(false)
+        } else {
+            txtRecursos.layer.borderColor = corBordaCerta
+            txtRecursos.layer.borderWidth = 0.25
+            arrayReturn.append(true)
+        }
+
+        if arrayReturn.contains(false) {
+            return false
+        } else {
+            return true
+        }
+    }
+    
+    func verificarTam() -> Bool {
+        let letras = NSCharacterSet.letters
+        if txtTamanho.text == "" || txtTamanho.text == nil {
+            txtTamanho.layer.borderColor = corBordaErrada
+            txtTamanho.layer.borderWidth = 2.0
+            txtTamanho.layer.cornerRadius = 6
+            return false
+        } else {
+            if let range = txtTamanho.text?.rangeOfCharacter(from: letras) {
+                txtTamanho.layer.borderColor = corBordaErrada
+                txtTamanho.layer.borderWidth = 2.0
+                txtTamanho.layer.cornerRadius = 6
+                return false
+            }
+            txtTamanho.layer.borderColor = corBordaCerta
+            txtTamanho.layer.borderWidth = 0.25
+            return true
+        }
     }
 }
 
