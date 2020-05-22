e62fb3a0c4b027e8d07ec7d655e9bf472c59e569
Tamara <tamara.erlij@gmail.com>
2020-05-22 15:03:53 -0300

    arrumando para evitar conflitos

--- ExperienceApp/Application/SceneDelegate.swift
+++ ExperienceApp/Application/SceneDelegate.swift
@@ -20,7 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
         self.window = UIWindow(windowScene: windowScene)
 
-        guard let rootVC = HomeViewController(nibName: "HomeViewController", bundle: nil) as? HomeViewController else {
+        guard let rootVC = TesteViewController(nibName: "TesteViewController", bundle: nil) as? TesteViewController else {
             print("ViewController not found")
             return
         }
