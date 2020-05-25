08c4b4df684190af7989e4b583d604382cf2cd69
Juliana Vigato Pavan <ju.vigato3@gmail.com>
2020-05-25 18:04:12 -0300

    começando nova tela

---
+++ ExperienceApp/Screen - Adm/Controller/TelaAdmController.swift
@@ -0,0 +1,9 @@
+//
+//  TelaAdmController.swift
+//  ExperienceApp
+//
+//  Created by Juliana Vigato Pavan on 25/05/20.
+//  Copyright © 2020 Zewu Chen. All rights reserved.
+//
+
+import Foundation
---
+++ ExperienceApp/Screen - Adm/View/TelaAdmViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TelaAdmViewController.swift
+//  ExperienceApp
+//
+//  Created by Juliana Vigato Pavan on 25/05/20.
+//  Copyright © 2020 Zewu Chen. All rights reserved.
+//
+
+import UIKit
+
+class TelaAdmViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
